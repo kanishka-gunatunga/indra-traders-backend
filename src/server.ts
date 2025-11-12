df5bb1b1515fc0372139751228afdd088583313d
Kaveesha Sulakshana <sulakshanakaveesha@gmail.com>
2025-11-12 14:38:34 +0530

    add websocket

--- src/server.ts
+++ src/server.ts
@@ -9,17 +9,17 @@ const PORT = process.env.PORT || 8081;
 const httpServer = http.createServer(app);
 
 // Attach socket server
-const io = new Server(httpServer, {
-    cors: { origin: "*" }
-});
+// const io = new Server(httpServer, {
+//     cors: { origin: "*" }
+// });
 
 // Initialize socket handlers
-initSocket(io);
+// initSocket(io);
 
 db.sequelize.sync({alter: false, force: false}).then(() => {
     console.log("Database synced successfully!!");
-    // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-    httpServer.listen(PORT, () => {
-        console.log(`Server + Socket.io running on port ${PORT}`);
-    });
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    // httpServer.listen(PORT, () => {
+    //     console.log(`Server + Socket.io running on port ${PORT}`);
+    // });
 });
