a34d12891bc498ac14a16a8927684f5c6cd3cc83
Kaveesha Sulakshana <sulakshanakaveesha@gmail.com>
2025-12-02 11:34:34 +0530

    implement some lead access filter

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use("/api/v1/uploads", express.static(path.join(process.cwd(), "public/uploads")));
+// app.use("/api/v1/uploads", express.static(path.join(process.cwd(), "public/uploads")));
 
 // const httpServer = http.createServer(app);
 // const io = new Server(httpServer, {
