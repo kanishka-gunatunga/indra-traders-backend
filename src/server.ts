6f90d4826ae3b71429e73d66a78351cf65f8935e
nethmidinanjana <nethmidinanjana02@gmail.com>
2026-01-16 15:15:45 +0530

    Developed branch details loading in login back-end route

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import db from "./models";
 import app from "./app";
 import http from "http";
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 // import initSocket from "./realtime/socket";
 import initSocket from "./realtime/indexSocket"
 
@@ -11,17 +11,17 @@ const httpServer = http.createServer(app);
 
 //Attach socket server
 export const io = new Server(httpServer, {
-    cors: {origin: "*"}
+    cors: { origin: "*" }
 });
 
 //Initialize socket handlers
 initSocket(io);
 
-db.sequelize.sync({alter: true, force: false}).then(() => {
-// db.sequelize.sync({alter: false, force: false}).then(() => {
+// db.sequelize.sync({alter: true, force: false}).then(() => {
+db.sequelize.sync({ alter: false, force: false }).then(() => {
     console.log("Database synced successfully!!");
     // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     httpServer.listen(PORT, () => {
         console.log(`Server + Socket.io running on port ${PORT}`);
-    });  
+    });
 });
